db32cf4acd969d9bd6f1345fab9785248d9efb7e
Tiffany Chen <tchen@Tiffanys-MacBook-Pro.local>
2014-07-26 11:05:41 -0500

    User  to load data

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
 var triviaApp = angular.module('triviaApp', []);
 
-triviaApp.controller('QuestionListCtrl', function($scope) {
-   $scope.questions = [
-       {
-           id: 1,
-           question: "Why is Angular JS named Angular JS?",
-           option1: "It sounds cool.",
-           option2: "It describes HTML brackets.",
-           option3: "The author had a habit of holding his head at an angle while coding.",
-           answer:"2"
-       },
-       {
-           id: 2,
-           question: "Why does AngularJS use the namespace ng?",
-           option1: "It stands for 'n gage' (engage).",
-           option2: "It's the initials of the original author.",
-           option3: "It sounds like angular.",
-           answer: "3"
-       }
-   ]
-});
+triviaApp.controller('QuestionListCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('data/questions.json').success(function(data) {
+       $scope.questions = data;
+    });
+}]);
